0be4e747ffa4b48e3ab9d5a6f80673f0f99828a0
hugolebihan56 <hugo56250@outlook.com>
2025-01-27 13:29:10 +0100

    zz

--- update.bat
+++ update.bat
@@ -1,10 +1,11 @@
 @echo off
 REM Se déplacer dans le dossier du projet
 
-REM Exécuter git pull
-echo Mise à jour du dépôt depuis GitHub...
-git pull https://github.com/hugolebihan56/recette-cuisine.git master
+REM Forcer le pull
+echo Forçage de la mise à jour du dépôt depuis GitHub...
+git fetch origin master
+git reset --hard origin/master
 
 REM Afficher un message de fin
-echo Mise à jour terminée !
+echo Mise à jour terminée avec succès !
 pause
